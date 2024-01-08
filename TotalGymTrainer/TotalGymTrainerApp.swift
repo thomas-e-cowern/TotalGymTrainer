33a6d9a01379e26c6eea7a25313b23c5df1f9d4b
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-01-08 08:17:25 -0500

    total gym trainer initial commit

---
+++ TotalGymTrainer/TotalGymTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalGymTrainerApp.swift
+//  TotalGymTrainer
+//
+//  Created by Thomas Cowern on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TotalGymTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
