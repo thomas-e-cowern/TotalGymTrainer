c93c58e45c1a51bfa7f0d1d59843800327deb99c
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-05 08:54:19 -0500

    add resources folder and symbols file to project

---
+++ TotalGymTrainer/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  TotalGymTrainer
+//
+//  Created by Thomas Cowern on 2/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let home = Image(systemName: "house.fill")
+    static let list = Image(systemName: "list.bullet")
+    static let workouts = Image(systemName: "figure.strengthtraining.functional")
+    static let settings = Image(systemName: "gear.circle.fill" )
+}
