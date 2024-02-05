917a3912d34a4efc7668a2bd9b260e672d4801ea
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-05 10:09:46 -0500

    add workout model

---
+++ TotalGymTrainer/Models/Workout.swift
@@ -0,0 +1,16 @@
+//
+//  Workout.swift
+//  TotalGymTrainer
+//
+//  Created by Thomas Cowern on 2/5/24.
+//
+
+import Foundation
+
+struct Workout {
+    let date: Date?
+    let exerciseName: String
+    let resistanceLevel: Int
+    let reps: Int
+    let sets: Int
+}
