033eae7af0c787fe7c9c5604c5510c1b8a558f46
Thomas Cowern <thomas.e.cowern@gmail.com>
2024-02-05 09:37:32 -0500

    add colors and theme file to project

---
+++ TotalGymTrainer/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  TotalGymTrainer
+//
+//  Created by Thomas Cowern on 2/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let buttonColor = Color("button")
+}
